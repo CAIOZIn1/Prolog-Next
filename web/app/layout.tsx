241c135dd6d56e73e0d82a0256f918047377e2a9
CAIOZin1 <sellacaio4@gmail.com>
2024-05-26 20:25:34 -0300

    :sparkles: feat: init do projeto web

---
+++ web/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Techonogy",
+  description: "Web site development by next",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
